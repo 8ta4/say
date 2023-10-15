132b060e0c74ebdf52dd115135d453e5dfa2dc22
a <a@as-Virtual-Machine.local>
2023-10-15 15:38:59 -0700

    Add Haskell support to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -17,6 +17,7 @@
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
+  languages.haskell.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;

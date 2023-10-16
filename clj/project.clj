11a2daec60cce9fb403c8cb78dac6cdcb42bce2f
a <a@as-Virtual-Machine.local>
2023-10-15 21:06:04 -0700

    Add libpython-clj and initialize Python

--- clj/project.clj
+++ clj/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[clj-python/libpython-clj "2.025"]
+                 [org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -1,7 +1,11 @@
 (ns clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [libpython-clj2.python :as py]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(py/initialize! :python-executable "../.venv/bin/python")

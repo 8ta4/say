00931b27d53c8b51ebb2baa6504fd039d4f9f84a
a <a@as-Virtual-Machine.local>
2023-10-15 17:03:43 -0700

    Update devenv.nix to include leiningen

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.leiningen
+    pkgs.git
+  ];
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";

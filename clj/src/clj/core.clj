ec88beb808331854eb738f66927493909ac5066f
a <a@as-Virtual-Machine.local>
2023-10-15 22:47:59 -0700

    Add audio recording configuration

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -15,3 +15,16 @@
 
 ; Record in chunks of 1024 samples
 (def chunk 1024)
+
+; 16 bits per sample
+(def sample-format (py/get-attr pyaudio "paInt16"))
+
+(def channels 1)
+; Record at 44100 samples per second
+(def fs 44100)
+
+(def seconds 3)
+
+(def filename "output.wav")
+
+(def p (py/call-attr pyaudio "PyAudio"))

528b1405a68fadb186c78d8952bebee18625d028
a <a@as-Virtual-Machine.local>
2023-10-19 16:30:30 -0700

    Add source links for chunk-size and fs values

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -19,6 +19,7 @@
   (println "Hello, World!"))
 
 ; TODO: Evaluate if chunk-size and fs values are optimal for the current use case
+; https://github.com/snakers4/silero-vad/blob/cb92cdd1e33cc1eb9c4ae3626bf3cd60fc660976/utils_vad.py#L207
 (def chunk-size 1536)
 
 ; 16 bits per sample
@@ -26,6 +27,7 @@
 
 (def channels 1)
 
+; https://github.com/snakers4/silero-vad/blob/cb92cdd1e33cc1eb9c4ae3626bf3cd60fc660976/utils_vad.py#L207
 (def fs 16000)
 
 (def filename "output.mp3")

51a28d65e72e27f96c62697885a5ba6f1079f1b1
a <a@as-Virtual-Machine.local>
2023-10-23 00:25:55 -0700

    Use http-conduit to make a GET request to localhost and run lein only if it fails

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -1,8 +1,15 @@
 module Main (main) where
 
+import Control.Exception (try)
 import Control.Monad (void)
+import Data.ByteString.Lazy (ByteString)
+import Network.HTTP.Simple
 import System.Process (spawnCommand)
 import Prelude
 
 main :: IO ()
-main = void (spawnCommand "cd ../clj && lein run")
+main = do
+  response <- try $ httpLBS "http://localhost:8080/" :: IO (Either HttpException (Response ByteString))
+  case response of
+    Left _ -> void (spawnCommand "cd ../clj && lein run")
+    Right _ -> pure ()

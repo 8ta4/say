27ce4efb46f04c7f80b4e904d9d128ae5c226bff
a <a@as-Virtual-Machine.local>
2023-10-27 01:40:27 -0700

    Implement build script using pipenv, leiningen, and stack

--- devenv.nix
+++ devenv.nix
@@ -18,6 +18,13 @@
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
+  scripts.build.exec = ''
+    ${pkgs.pipenv}/bin/pipenv install
+    cd "$DEVENV_ROOT/clj"
+    ${pkgs.leiningen}/bin/lein uberjar
+    cd "$DEVENV_ROOT/hs"
+    ${pkgs.haskellPackages.stack}/bin/stack install --local-bin-path .
+  '';
   scripts.run.exec = ''
     cd "$DEVENV_ROOT/hs"
     ${pkgs.ghcid}/bin/ghcid --command="${pkgs.stack}/bin/stack ghci" -T="main" --warnings

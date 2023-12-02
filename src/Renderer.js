a1fa67b39dd5bfe69228d5e4b43c3c3c2f67a008
a <a@as-Virtual-Machine.local>
2023-12-01 19:29:17 -0800

    Add audio worklet module to audio context

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,4 +1,5 @@
 export async function record() {
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
   const context = new AudioContext();
+  await context.audioWorklet.addModule("audio.js");
 }

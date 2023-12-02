e23b8c9707a47269a6819f5e65dbf4ac3b012820
a <a@as-Virtual-Machine.local>
2023-12-01 16:25:23 -0800

    Replace Electron app with PureScript output module

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// https://www.electronjs.org/docs/latest/tutorial/quick-start#create-a-web-page
-const { app, BrowserWindow, globalShortcut } = require("electron");
-
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-
-  win.loadFile("index.html");
-};
-
-app.whenReady().then(() => {
-  createWindow();
-
-  globalShortcut.register("Command+;", () => {
-    console.log("Command+; is pressed");
-  });
+import("./output/Main/index.js").then((module) => {
+  module.main();
 });

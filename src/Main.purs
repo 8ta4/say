97f65474daeab41942c50ad47f15aa5615e7b989
a <a@as-Virtual-Machine.local>
2023-12-07 14:52:54 -0800

    Reorder functions in Main.purs for readability

--- src/Main.purs
+++ src/Main.purs
@@ -9,13 +9,6 @@ import Node.Stream (Readable, pipe)
 
 foreign import data Float32Array :: Type
 
-createStream :: Effect (Readable ())
-createStream = do
-  stream <- newReadable
-  ffmpeg <- spawn "ffmpeg" [ "-y", "-f", "f32le", "-ar", "16000", "-i", "pipe:0", "-ar", "16000", "output.opus" ] defaultSpawnOptions
-  _ <- pipe stream $ stdin ffmpeg
-  pure stream
-
 main :: Effect Unit
 main = do
   stream <- createStream
@@ -36,13 +29,16 @@ main = do
       foo state.buffer
   launch record process
 
+createStream :: Effect (Readable ())
+createStream = do
+  stream <- newReadable
+  ffmpeg <- spawn "ffmpeg" [ "-y", "-f", "f32le", "-ar", "16000", "-i", "pipe:0", "-ar", "16000", "output.opus" ] defaultSpawnOptions
+  _ <- pipe stream $ stdin ffmpeg
+  pure stream
+
 foreign import newReadable :: Effect (Readable ())
 
 foreign import pushAudio :: Readable () -> Float32Array -> Effect Unit
-
-foreign import end :: Readable () -> Effect Unit
-
-foreign import launch :: (Float32Array -> Effect Unit) -> Effect Unit -> Effect Unit
 
 foreign import appendFloat32Array :: Float32Array -> Float32Array -> Float32Array
 
@@ -53,5 +49,9 @@ foreign import memptyFloat32Array :: Float32Array
 
 instance monoidFloat32Array :: Monoid Float32Array where
   mempty = memptyFloat32Array
+
+foreign import end :: Readable () -> Effect Unit
+
+foreign import launch :: (Float32Array -> Effect Unit) -> Effect Unit -> Effect Unit
 
 foreign import foo :: Float32Array -> Effect Unit

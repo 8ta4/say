54b051e2b67a431545dd7a92ac24195bab023ab8
a <a@as-Virtual-Machine.local>
2023-12-09 16:20:54 -0800

    Simplify detect function signature and move it to the main function scope

--- src/Main.purs
+++ src/Main.purs
@@ -6,17 +6,15 @@ import Data.Int (floor, toNumber)
 import Data.UUID (genUUID, toString)
 import Debug (traceM)
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_)
+import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
-import Effect.Ref (Ref, new, read, write)
+import Effect.Ref (new, read, write)
 import Float32Array (Float32Array, length, splitAt, takeEnd)
 import Node.ChildProcess (ChildProcess, defaultSpawnOptions, spawn, stdin)
 import Node.FS.Sync (mkdtemp)
 import Node.OS (tmpdir)
-import Node.Stream (Read, Readable, Stream, pipe)
+import Node.Stream (Readable, pipe)
 import Promise.Aff (Promise, toAffE)
-
-type StateRef r = Ref { stream :: Stream (read :: Read), streamLength :: Int, pause :: Float32Array, h :: Tensor, c :: Tensor | r }
 
 foreign import data Tensor :: Type
 
@@ -28,6 +26,18 @@ main = do
   stream <- createStream appTempDirectory
   ref <- new { stream: stream, pause: mempty, streamLength: 0, raw: mempty, h: tensor, c: tensor }
   let
+    detect audio = do
+      state <- liftEffect $ read ref
+      result <- toAffE $ run audio state.h state.c
+      let state' = state { h = result.h, c = result.c }
+
+      -- https://github.com/snakers4/silero-vad/blob/5e7ee10ee065ab2b98751dd82b28e3c6360e19aa/utils_vad.py#L187-L188
+      if (0.5 < result.probability) then do
+        liftEffect $ write (state' { streamLength = state.streamLength + length state.pause, pause = mempty }) ref
+        liftEffect $ push state.stream $ state.pause
+      else
+        liftEffect $ write state' ref
+  let
     record = \audio -> do
 
       -- TODO: Add your audio recording logic here
@@ -38,7 +48,7 @@ main = do
       if length raw >= windowSizeSamples then do
         let { before, after } = splitAt windowSizeSamples raw
         write (state { raw = after, pause = takeEnd samplesInPause $ state.pause <> before }) ref
-        launchAff_ $ detect ref before
+        launchAff_ $ detect before
       else
         write (state { raw = raw }) ref
   let
@@ -78,19 +88,6 @@ pauseDuration = 1.5
 
 samplesInPause :: Int
 samplesInPause = floor $ toNumber ar * pauseDuration
-
-detect :: forall r. StateRef r -> Float32Array -> Aff Unit
-detect ref audio = do
-  state <- liftEffect $ read ref
-  result <- toAffE $ run audio state.h state.c
-  let state' = state { h = result.h, c = result.c }
-
-  -- https://github.com/snakers4/silero-vad/blob/5e7ee10ee065ab2b98751dd82b28e3c6360e19aa/utils_vad.py#L187-L188
-  if (0.5 < result.probability) then do
-    liftEffect $ write (state' { streamLength = state.streamLength + length state.pause, pause = mempty }) ref
-    liftEffect $ push state.stream $ state.pause
-  else
-    liftEffect $ write state' ref
 
 foreign import run :: Float32Array -> Tensor -> Tensor -> Effect (Promise { probability :: Number, h :: Tensor, c :: Tensor })
 

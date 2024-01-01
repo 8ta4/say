8278c1bcbe670d78af614b47279d8e3003550e69
a <a@as-Virtual-Machine.local>
2024-01-01 15:54:21 -0800

    Add purescript-psa to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -12,13 +12,14 @@
 
     # https://github.com/NixOS/nixpkgs/issues/253198
     # The package spago-0.20.9 is marked as broken in the Nix packages repository, which caused the error.
-    # https://github.com/cachix/devenv/blob/7354096fc026f79645fdac73e9aeea71a09412c3/src/modules/languages/purescript.nix#L28-L29
+    # https://github.com/cachix/devenv/blob/7354096fc026f79645fdac73e9aeea71a09412c3/src/modules/languages/purescript.nix#L28-L30
     # https://github.com/cachix/devenv/blob/7354096fc026f79645fdac73e9aeea71a09412c3/src/modules/languages/purescript.nix#L18
     # I am using `yarn` to install `spago` due to issues encountered when trying to use `pkgs.spago` and `languages.purescript.enable`.
     # https://github.com/electron-userland/electron-builder/blob/47e66ca64a89395a49300e8b2da1d9baeb93825a/docs/index.md?plain=1#L33
     pkgs.nodePackages.purescript-language-server
     pkgs.nodePackages.purs-tidy
     pkgs.purescript
+    pkgs.purescript-psa
     pkgs.yarn-berry
   ];
 

60dd8db47a0ee24e1f46854ee34a4fcd4d9bdcc0
a <a@as-Virtual-Machine.local>
2024-01-26 15:33:17 -0800

    Find and trace microphone device ID in main function

--- src/Renderer.purs
+++ src/Renderer.purs
@@ -2,6 +2,9 @@ module Renderer where
 
 import Prelude
 
+import Data.Array (find)
+import Data.Maybe (Maybe(..))
+import Debug (traceM)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Promise.Aff (Promise, toAffE)
@@ -9,6 +12,10 @@ import Promise.Aff (Promise, toAffE)
 main :: Effect Unit
 main = launchAff_ $ do
   devices <- toAffE getDevices
+  let microphoneDevice = find (\device -> device.label == "Microphone (Built-in)") devices
+  case microphoneDevice of
+    Just device -> traceM device.deviceId
+    Nothing -> traceM "Microphone (Built-in) not found"
   toAffE record
 
 foreign import getDevices :: Effect (Promise (Array { deviceId :: String, label :: String }))

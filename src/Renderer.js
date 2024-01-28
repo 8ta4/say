99141c8d5098ea59b59cd3517de233e17a99f20c
a <a@as-Virtual-Machine.local>
2024-01-27 18:25:03 -0800

    Add audioContext and handle functions and update record function to close old context

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,5 +1,7 @@
 const { ipcRenderer } = require("electron");
 
+export const audioContext = () => new AudioContext({ sampleRate: 16000 });
+
 export async function getDevices() {
   await navigator.mediaDevices.getUserMedia({ audio: true });
   return navigator.mediaDevices.enumerateDevices();
@@ -8,16 +10,21 @@ export async function getDevices() {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith#searchstring
 export const endsWith = (searchString) => (str) => str.endsWith(searchString);
 
-export const record = (deviceId) => async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: { deviceId: { exact: deviceId } },
-  });
-  const context = new AudioContext({ sampleRate: 16000 });
-  await context.audioWorklet.addModule("audio.js");
-  const source = context.createMediaStreamSource(stream);
-  const processor = new AudioWorkletNode(context, "processor");
-  source.connect(processor);
-  processor.port.onmessage = (event) => {
-    ipcRenderer.send("audio", event.data);
+export const record =
+  (deviceId) => (oldContext) => (newContext) => async () => {
+    oldContext.close();
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: { deviceId: { exact: deviceId } },
+    });
+    await newContext.audioWorklet.addModule("audio.js");
+    const source = newContext.createMediaStreamSource(stream);
+    const processor = new AudioWorkletNode(newContext, "processor");
+    source.connect(processor);
+    processor.port.onmessage = (event) => {
+      ipcRenderer.send("audio", event.data);
+    };
   };
+
+export const handle = (process) => () => {
+  navigator.mediaDevices.ondevicechange = process;
 };
--- src/Renderer.purs
+++ src/Renderer.purs
@@ -7,18 +7,35 @@ import Data.Maybe (Maybe(..))
 import Debug (traceM)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Effect.Ref (new, read, write)
 import Promise.Aff (Promise, toAffE)
 
+foreign import data AudioContext :: Type
+
 main :: Effect Unit
-main = launchAff_ $ do
-  devices <- toAffE getDevices
-  let microphoneDevice = find (\device -> endsWith "(Built-in)" device.label) devices
-  case microphoneDevice of
-    Just device -> toAffE $ record device.deviceId
-    Nothing -> traceM "(Built-in) not found"
+main = do
+  context <- audioContext
+  ref <- new context
+  let
+    process = do
+      context' <- read ref
+      context'' <- audioContext
+      write context'' ref
+      launchAff_ $ do
+        devices <- toAffE getDevices
+        let microphoneDevice = find (\device -> endsWith "(Built-in)" device.label) devices
+        case microphoneDevice of
+          Just device -> toAffE $ record device.deviceId context' context''
+          Nothing -> traceM "(Built-in) not found"
+  process
+  handle process
+
+foreign import audioContext :: Effect AudioContext
 
 foreign import getDevices :: Effect (Promise (Array { deviceId :: String, label :: String }))
 
 foreign import endsWith :: String -> String -> Boolean
 
-foreign import record :: String -> Effect (Promise Unit)
+foreign import record :: String -> AudioContext -> AudioContext -> Effect (Promise Unit)
+
+foreign import handle :: Effect Unit -> Effect Unit

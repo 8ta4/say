fda762e6ee24d4e16133b91a25e942942efeefb0
a <a@as-Virtual-Machine.local>
2024-02-21 17:28:50 -0800

    Integrate `whenReady` promise to create the window after the app is ready

--- src/main.cljs
+++ src/main.cljs
@@ -1,10 +1,11 @@
 (ns main
-  (:require [electron :refer [BrowserWindow]]))
-
-(defn main []
-  (println "Hello, Electron!"))
+  (:require [electron :refer [app BrowserWindow]]))
 
 (defn create-window
   []
   (let [win (BrowserWindow. #js {:width 800, :height 600})]
     (.loadFile win "public/index.html")))
+
+(defn main []
+  (println "Hello, Electron!")
+  (.then (.whenReady app) (fn [] (create-window))))

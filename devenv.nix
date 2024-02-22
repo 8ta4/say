df60783bf1c4fc5061e13b51faa6013475a22806
a <a@as-Virtual-Machine.local>
2024-02-21 16:15:50 -0800

    Add build script for Shadow CLJS compilation

--- devenv.nix
+++ devenv.nix
@@ -18,6 +18,7 @@
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
   scripts.run.exec = "electron .";
+  scripts.build.exec = "shadow-cljs compile :main";
 
   enterShell = ''
     hello

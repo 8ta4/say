8ef653d30f82b288b02856dd916e940a1c07f997
a <a@as-Virtual-Machine.local>
2024-02-22 16:50:16 -0800

    Initialize a persistent root element for rendering

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -1,7 +1,12 @@
 (ns renderer
   (:require [reagent.dom.client :as client]))
 
+(defonce root
+  ;; Using defonce to ensure the root is only created once. This prevents warnings about
+  ;; calling ReactDOMClient.createRoot() on a container that has already been passed to
+  ;; createRoot() before during hot reloads or re-evaluations of the code.
+  (client/create-root (js/document.getElementById "app")))
+
 (defn init []
-  (client/render (client/create-root (js/document.getElementById "app"))
-                 [:div "Hello, Renderer!"])
+  (client/render root [:div "Hello, Renderer!"])
   (println "Hello, Renderer!"))

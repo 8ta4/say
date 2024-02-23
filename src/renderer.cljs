a7d954364bcea0d3bca756cbbc545f1c7da9cc51
a <a@as-Virtual-Machine.local>
2024-02-22 19:27:35 -0800

    Store secrets in YAML format at user’s home directory configuration path in renderer.cljs

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -1,5 +1,7 @@
 (ns renderer
   (:require ["@mui/material/TextField" :default TextField]
+            ["yaml" :as yaml]
+            [cljs-node-io.core :refer [spit]]
             [com.rpl.specter :as specter]
             [reagent.core :as reagent]
             [reagent.dom.client :as client]))
@@ -22,13 +24,14 @@
 
 (defonce secrets (reagent/atom {:key ""}))
 
+(def secrets-path (path.join (os.homedir) ".config/say/secrets.yaml"))
+
 (defn init []
   (client/render root [:> TextField {:label "Deepgram API Key"
                                      :type "password"
                                      :on-change (fn [event]
                                                   (specter/setval [specter/ATOM :key] event.target.value secrets))}])
-  (add-watch secrets :change (fn [_ _ _ _]
-                               (js/console.log "Secrets updated")))
+  (add-watch secrets :change (fn [_ _ _ secrets*]
+                               (js/console.log "Secrets updated")
+                               (spit secrets-path (yaml/stringify (clj->js secrets*)))))
   (println "Hello, Renderer!"))
-
-(def secrets-path (path.join (os.homedir) ".config/say/secrets.yaml"))

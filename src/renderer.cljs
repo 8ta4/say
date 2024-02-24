c520020488e2d7a9c9280cb241c1769126a35b4c
a <a@as-Virtual-Machine.local>
2024-02-23 18:15:22 -0800

    Define window-size-samples with a value of 1536

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -74,3 +74,7 @@
     (let [data (async/<! chan)]
       (specter/transform [specter/ATOM :raw] (fn [raw] (append-float-32-array raw data)) state)
       (recur))))
+
+;; https://github.com/snakers4/silero-vad/blob/5e7ee10ee065ab2b98751dd82b28e3c6360e19aa/utils_vad.py#L207
+(def window-size-samples
+  1536)

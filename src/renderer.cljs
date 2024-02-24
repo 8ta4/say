36c703d7c9ba3f01310e19ccc73a651ba4ef7581
a <a@as-Virtual-Machine.local>
2024-02-23 17:55:04 -0800

    Move comment to precede defonce for root creation

--- src/renderer.cljs
+++ src/renderer.cljs
@@ -22,10 +22,10 @@
 (def fs
   (js/require "fs"))
 
+;; Using defonce to ensure the root is only created once. This prevents warnings about
+;; calling ReactDOMClient.createRoot() on a container that has already been passed to
+;; createRoot() before during hot reloads or re-evaluations of the code.
 (defonce root
-  ;; Using defonce to ensure the root is only created once. This prevents warnings about
-  ;; calling ReactDOMClient.createRoot() on a container that has already been passed to
-  ;; createRoot() before during hot reloads or re-evaluations of the code.
   (client/create-root (js/document.getElementById "app")))
 
 (defonce secrets (reagent/atom {:key ""}))

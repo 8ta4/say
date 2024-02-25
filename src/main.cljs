d7340e7081e142aca989f83339734a85566015df
a <a@as-Virtual-Machine.local>
2024-02-25 13:49:55 -0800

    Define a global shortcut variable for command registration

--- src/main.cljs
+++ src/main.cljs
@@ -1,6 +1,9 @@
 (ns main
   (:require [electron :refer [app BrowserWindow globalShortcut]]))
 
+(def shortcut
+  "Command+;")
+
 (defn initializeWindow []
   (let [win (BrowserWindow. (clj->js {:width 800
                                       :height 600
@@ -9,9 +12,9 @@
                                                        ;; https://stackoverflow.com/questions/44391448/electron-require-is-not-defined#:~:text=without%20the%20line%20contextisolation%3A%20false%2C%20you%20won't%20be%20able%20to%20access%20require
                                                        :contextIsolation false}}))]
     (.loadFile win "public/index.html")
-    (.register globalShortcut "Command+;" (fn []
-                                            (js/console.log "Command+; is pressed")
-                                            (.webContents.send win "shortcut")))))
+    (.register globalShortcut shortcut (fn []
+                                         (js/console.log "Global shortcut" shortcut "pressed.")
+                                         (.webContents.send win "shortcut")))))
 
 (defn main []
   (js/console.log "Hello, Electron!")

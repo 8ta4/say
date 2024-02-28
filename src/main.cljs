2617e834ad5e40a17fa080d41f1350bd52b878e6
a <a@as-Virtual-Machine.local>
2024-02-27 19:05:49 -0800

    Use electron namespace instead of refer

--- src/main.cljs
+++ src/main.cljs
@@ -1,22 +1,22 @@
 (ns main
-  (:require [electron :refer [app BrowserWindow globalShortcut]]
+  (:require [electron]
             [shared :refer [channel]]))
 
 (def shortcut
   "Command+;")
 
 (defn initialize-window []
-  (let [win (BrowserWindow. (clj->js {:width 800
-                                      :height 600
-                                      :webPreferences {:nodeIntegration true
+  (let [win (electron/BrowserWindow. (clj->js {:width 800
+                                               :height 600
+                                               :webPreferences {:nodeIntegration true
 
                                                        ;; https://stackoverflow.com/questions/44391448/electron-require-is-not-defined#:~:text=without%20the%20line%20contextisolation%3A%20false%2C%20you%20won't%20be%20able%20to%20access%20require
-                                                       :contextIsolation false}}))]
+                                                                :contextIsolation false}}))]
     (.loadFile win "public/index.html")
-    (.register globalShortcut shortcut (fn []
-                                         (js/console.log "Global shortcut" shortcut "pressed.")
-                                         (.webContents.send win channel)))))
+    (electron/globalShortcut.register  shortcut (fn []
+                                                  (js/console.log "Global shortcut" shortcut "pressed.")
+                                                  (.webContents.send win channel)))))
 
 (defn main []
   (js/console.log "Hello, Electron!")
-  (.then (.whenReady app) initialize-window))
+  (.then (electron/app.whenReady) initialize-window))

05191e1d2198b149b35f3a4468021936827df9e2
a <a@as-Virtual-Machine.local>
2024-03-08 17:59:56 -0800

    Add background flag and dock icon hiding logic

--- src/main.cljs
+++ src/main.cljs
@@ -6,13 +6,18 @@
 (def shortcut
   "Command+;")
 
+(def background
+  (= "--background" (last process/argv)))
+
 (defn initialize []
+  (when background
+    (electron/app.dock.hide))
   (let [win (electron/BrowserWindow. (clj->js {:width 800
                                                :height 600
                                                :webPreferences {:nodeIntegration true
 ;; https://stackoverflow.com/questions/44391448/electron-require-is-not-defined#:~:text=without%20the%20line%20contextisolation%3A%20false%2C%20you%20won't%20be%20able%20to%20access%20require
                                                                 :contextIsolation false}
-                                               :show (not= "--background" (last process/argv))}))]
+                                               :show (not background)}))]
     (.loadFile win "public/index.html")
     (electron/globalShortcut.register shortcut (fn []
                                                  (js/console.log "Global shortcut" shortcut "pressed.")
